e3809550d3fee2d355b0850071d7210072609b64
Fabio Araujo <fabioaraujods@gmail.com>
2020-07-12 10:45:58 -0300

    alterado código do servidor para utilizar o módulo express

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
-const http = require('http');
+//recupera uma funlção express
+const express = require('express');
+//executa a função recebida do required e guarda o retorno na constante app
+const app = express();
 
-const servidor  = http.createServer(function (req, resp) {
-    let html = '';
-    if (req.url == '/') {
-        html = `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Casa do Código </h1>
-            </body> 
-        </html>
-    `;
-    } else if (req.url == '/livros')
-    html = `
-    <html>
-        <head>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1> Listagem de livros </h1>
-        </body> 
-    </html>
-`;
-    resp.end(html);
+app.listen(3000, function(){
+    console.log('Servidor rodando na porta 3000.');
 });
 
-servidor.listen(3000);
+app.get('/', function(req, resp) {
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Casa do Código </h1>
+                </body> 
+            </html>
+        `
+    );
+});
+
+app.get('/livros', function(req, resp) {
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Listagem de livros </h1>
+                </body> 
+            </html>
+        `
+    );
+});
