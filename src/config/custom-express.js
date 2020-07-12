7d9e4646830b39fc476b9254d95a6d760bd2fbd0
Fabio Araujo <fabioaraujods@gmail.com>
2020-07-12 11:14:01 -0300

    incluída biblioteca marko para templates HTML

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,3 +1,6 @@
+require('marko/node-require').install();
+require('marko/express');
+
 //recupera uma funlção express
 const express = require('express');
 //executa a função recebida do required e guarda o retorno na constante app
