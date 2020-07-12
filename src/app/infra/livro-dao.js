26958219ec39bf2f5bdfbdde77a9b0ff9bff9153
Fabio Araujo <fabioaraujods@gmail.com>
2020-07-12 15:08:15 -0300

    alterado DAO de callback para utilizar Promises

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -4,19 +4,19 @@ class LivroDao {
         this._db = db;
     }
 
-    lista(callback) {
-        this._db.all(
-            'SELECT * FROM livros',
-            (erro, resultados) =>
-                callback(erro, resultados)
-                /* 
-                a arrow function acima é o mesmo que a declaração abaixo
-            function(erro, resultados) {
-                callback(erro, resultados);
-            }     
-            */   
-        )
+    lista() {
+        return new Promise((resolve, reject) => {
+            this._db.all(
+                'SELECT * FROM livros',
+                (erro, resultados) => {
+                    if (erro) 
+                    return reject('Não foi possível listar os livros!');
 
+                    return resolve(resultados);
+                }
+            )
+
+        });
     }
 
 }
