ded1ff1bfb021d270d2396f1b324affa80d9453b
Fabio Araujo <fabioaraujods@gmail.com>
2020-07-12 11:01:08 -0300

    separando as responsabilidades do arquivo server.js

--- server.js
+++ server.js
@@ -1,38 +1,6 @@
-//recupera uma funlção express
-const express = require('express');
-//executa a função recebida do required e guarda o retorno na constante app
-const app = express();
+const app = require('./src/config/custom-express');
 
 app.listen(3000, function(){
     console.log('Servidor rodando na porta 3000.');
 });
 
-app.get('/', function(req, resp) {
-    resp.send(
-        `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Casa do Código </h1>
-                </body> 
-            </html>
-        `
-    );
-});
-
-app.get('/livros', function(req, resp) {
-    resp.send(
-        `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Listagem de livros </h1>
-                </body> 
-            </html>
-        `
-    );
-});
---
+++ src/config/custom-express.js
@@ -0,0 +1,9 @@
+//recupera uma funlção express
+const express = require('express');
+//executa a função recebida do required e guarda o retorno na constante app
+const app = express();
+
+const rotas = require('../app/rotas/rotas.js');
+rotas(app);
+
+module.exports = app;
