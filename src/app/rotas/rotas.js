9ed3ca94dc872862520c4135b34b753e3be44823
Fabio Araujo <fabioaraujods@gmail.com>
2020-07-12 11:48:41 -0300

    incluído banco de dados sqlite3

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,3 +1,8 @@
+const db = require('../../config/database');
+
+const sqlite3 = require('sqlite3').verbose();
+const bd = new sqlite3.Database('data.db');
+
 module.exports = (app) => {
 
     app.get('/', function(req, resp) {
@@ -16,20 +21,14 @@ module.exports = (app) => {
     });
 
     app.get('/livros', function(req, resp) {
-        resp.marko(
-            require('../views/livros/lista/lista.marko'),
-            {
-                livros: [
-                    { 
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    { 
-                        id: 2,
-                        titulo: 'Node Avançado'
-                    }
-                ]
-            }
-        );
+        db.all('SELECT * FROM livros', function(erro, resultados) {
+            resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: resultados
+                }
+            );
+        });
+        
     });
 }
